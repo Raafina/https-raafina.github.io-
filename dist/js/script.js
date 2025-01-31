67ff025d89c1fe85a5b43b746b50efaa96eef15f
Raafina <raafinuradzani16@gmail.com>
2025-01-31 08:31:51 +0700

    add button: back to top

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,35 @@
 // navbar
 const header = document.querySelector('header');
 const fixNav = header.offsetTop;
+const toTop = document.querySelector('#toTop');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > fixNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   } else {
     header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
 });
 
 // hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
+});
+
+// click outside hamburger
+window.addEventListener('click', (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
 });
 
 // send email
